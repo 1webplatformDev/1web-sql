a6d1c29b0ffa77ba2d4d08b8034d3633c027f4a4
Maksim <email@mail.ru>
2023-09-23 09:41:41 +0400

    add commit table

--- constuctor/type_component.sql
+++ constuctor/type_component.sql
@@ -11,6 +11,8 @@ create table constuctor.type_component (
 	const_name varchar not null -- 'Программное название типа компонента'
 );
 
+comment on table constuctor.type_component IS 'Тип компонента';
+
 create unique index type_component_const_name_idx on constuctor.type_component using btree (const_name);
 create unique index type_component_name_idx on constuctor.type_component using btree (name);
 
