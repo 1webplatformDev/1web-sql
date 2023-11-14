78e98756dcab36ad6d9b8818ca9f825f15d143ed
Semenov <Semenov@mail.ru>
2023-11-14 22:29:06 +0400

    todo 5-создание функции которая сравнивает 2 массива

--- public/index.sql
+++ public/index.sql
@@ -1,1 +1,17 @@
 create schema public authorization postgres;
+
+drop function public.intersectAndExcept;
+-- Функция принимает 2 массива что передал пользователь и что вернула функция
+-- в element_intersect хранятся валидные переданные пользователем значения
+-- в element_except хранятся значения которых нет в базе данных и они считаются не валидными
+create or replace function public.intersectAndExcept(array_ int4[], array_ids int4[])
+	returns table (element_intersect int4[], element_except int4[])
+	LANGUAGE plpgsql
+AS $function$
+	begin
+		return query 
+		select 
+			array(select unnest(array_) intersect select unnest(array_ids)) as id_save, 
+			array(select unnest(array_) except select unnest(array_ids)) as id_error;
+	end;
+$function$;
