d3247fcc9325d01bc60accb53cb39cd00455166e
Semenov <Semenov@mail.ru>
2023-11-19 11:24:53 +0400

    24-функция получения функции и входящих параметров

--- constructor/index.sql
+++ constructor/index.sql
@@ -1,2 +1,2 @@
 create schema constructor authorization postgres;
-comment on schema constructor is 'Схема для работы с конструктором';
+comment on schema constructor is 'Схема для работы с конструктором (для разработчиков)';
---
+++ tec/index.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA tec AUTHORIZATION postgres;
+COMMENT ON SCHEMA tec IS 'Техническая схема (для разработчиков)';
+
+drop function if exists tec.get_fun_in_params;
+create or replace function tec.get_fun_in_params(
+    schema_ varchar[],
+    entity_ varchar
+)
+	returns table (fun_name information_schema.sql_identifier, params jsonb)
+	language plpgsql
+	as $function$
+	begin
+        return query select r.routine_name as fun_name,
+            jsonb_agg(
+                jsonb_build_object(
+                    'name', p.parameter_name,
+                    'type', p.data_type 
+                )
+            ) as params
+        from information_schema.routines r
+        left join information_schema.parameters p on (r.specific_name = p.specific_name)
+        where r.routine_type = 'FUNCTION'
+        and r.routine_schema = any(schema_)
+        and p.parameter_mode = 'IN'
+        and r.routine_name like(entity_ || '%')
+        group by fun_name;
+	end;
+$function$;
